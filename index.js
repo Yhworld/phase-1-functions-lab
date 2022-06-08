1ecbc54d74ea60d9551d7c66432a06ef51658b81
Yahya <yahyahussein670@gmail.com>
2022-06-08 10:51:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(distance){
-    if (distance > 42){
-    return distance - 42;
-    }else{
-        return 42 - distance;
+function distanceFromHqInBlocks(distance) {
+    //returns the number of blocks given a value
+    if (distance > 42) {
+        return distance - 42
+    }
+    else {
+        return 42 - distance
+    }
+
+}
+
+function distanceFromHqInFeet(feet) {
+    return distanceFromHqInBlocks(feet)*264
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlock
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    if (start < destination) {
+        return Math.abs(destination - start )*264
+    } else {
+        return Math.abs(destination - start )*264
     }
 }
 
-function distanceFromHqInFeet(distance){
-        return distanceFromHqInBlocks(distance) * 264;
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    let totalDistance = distanceTravelledInFeet(start , destination);
 
-}
-
-function distanceTravelledInFeet(start, destination){
-    if (start < destination){
-    return (destination - start)* 264;
-    }else{
-        return (start - destination) * 264;
+    if (totalDistance <= 400) {
+        return 0;
     }
-}
-
-function calculatesFarePrice(start, destination){
-    let distance = distanceTravelledInFeet(start, destination);
-    if (distance <= 400){
-        return distance * 0;
-    }else if(distance >400 && distance <= 2000){
-        return (distance - 400) * .02;
-    }else if(distance > 2000 && distance < 2500){
+    else if (totalDistance > 400 && totalDistance <= 2000) {
+        return (totalDistance - 400) * 0.02
+    } else if (totalDistance > 2000 && totalDistance <= 2500) {
         return 25;
-    }else {
-        return 'cannot travel that far';
-}
-}
+    } else (totalDistance > 2500); {
+        return 'cannot travel that far'
+    }
+  }
